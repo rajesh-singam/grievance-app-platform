783c212cbedf793bdb73c7f5998a65f4024c27b6
Rajesh <rajesh@example.com>
2026-08-18 04:01:22 +0000

    Adding terraform modules for Infra code management

---
+++ terraform/modules/eks/variables.tf
@@ -0,0 +1,14 @@
+variable "cluster_name" {
+  description = "EKS cluster name"
+  type        = string
+}
+
+variable "vpc_id" {
+  description = "VPC ID"
+  type        = string
+}
+
+variable "public_subnet_ids" {
+  description = "Public subnet IDs for the EKS cluster and node group"
+  type        = list(string)
+}
